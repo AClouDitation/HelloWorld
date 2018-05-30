6604f111af2308e5c9439033c080655d64601afb
aclouditation <25907312+AClouDitation@users.noreply.github.com>
2018-05-30 14:40:50 -0400

    finally works

--- test.c
+++ test.c
@@ -8,26 +8,21 @@
 
 static uint8_t buf[] = {
 	0x90, 											//nop 
+	0x48, 0x89, 0xD6,								//mov 	rsi rdx,
 	0x48, 0xC7, 0xC0, 0x01, 0x00, 0x00, 0x00,		//mov	rax,0x1
 	0x48, 0xC7, 0xC2, 0x0D, 0x00, 0x00, 0x00,		//mov 	rdx,0xd
-	0x48, 0x8B, 0x34, 0x25, 0x00, 0x00, 0x00, 0x00,	//rsi, QWORD PTR ds: TBA
 	0x48, 0xC7, 0xC7, 0x01, 0x00, 0x00, 0x00,		//mov 	rdi,0x1
 	0x0F, 0x05,										//syscall
-	0xc3											//ret
+	0xC3											//ret
 };
 
-typedef int (*func_t)(void);
 const char message[] = "Hello World!\n";
+typedef int (*func_t)(unsigned long);
+
 int main(){
 
 	const size_t len = sizeof(buf);
-	
-	uint8_t addr[4] = {0};
-	addr[0] = (uint32_t)message & 0xFF;
-	addr[1] = ((uint32_t)message >> 8) & 0xFF;
-	addr[2] = ((uint32_t)message >> 16) & 0xFF;
-	addr[3] = ((uint32_t)message >> 24) & 0xFF;
-	memcpy(buf + 19, addr, 4);
+	unsigned long addr = (unsigned long)message;
 
 	void *hw_code = mmap(NULL, len, PROT_READ|PROT_WRITE, 
 		MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
@@ -44,9 +39,9 @@ int main(){
 		return EXIT_FAILURE;
 	}
 
-	func_t func = (func_t)(long)hw_code;
-	func();
-	printf("Hello World!\n");
+	func_t func = (func_t)(uint64_t)hw_code;
+	func(addr);
+	printf("Done!\n");
 
 	return 0;
 }
