e318c189cd87f767a25512e22bfc96b8f4a42bf7
aclouditation <25907312+AClouDitation@users.noreply.github.com>
2018-05-30 15:41:04 -0400

    redesigned for simplicity

--- test.c
+++ test.c
@@ -7,41 +7,52 @@
 #include <byteswap.h>
 
 static uint8_t buf[] = {
-	0x90, 											//nop 
-	0x48, 0x89, 0xD6,								//mov 	rsi rdx,
 	0x48, 0xC7, 0xC0, 0x01, 0x00, 0x00, 0x00,		//mov	rax,0x1
 	0x48, 0xC7, 0xC2, 0x0D, 0x00, 0x00, 0x00,		//mov 	rdx,0xd
+	0x48, 0xC7, 0xC6, 0x00, 0x00, 0x00, 0x00,		//mov 	rsi,TBA  
 	0x48, 0xC7, 0xC7, 0x01, 0x00, 0x00, 0x00,		//mov 	rdi,0x1
 	0x0F, 0x05,										//syscall
 	0xC3											//ret
 };
 
-const char message[] = "Hello World!\n";
-typedef int (*func_t)(unsigned long);
+typedef int (*func_t)(void);
+
+int print(char* message){
+
+	/* write message address */
+	uint32_t *addr = (uint32_t*)&message;
+	memcpy(buf + 17,(uint8_t*)addr,4);
+
+	/* write message length */
+	size_t len = strlen(message);
+	uint32_t *len_ptr = (uint32_t*)&len;
+	memcpy(buf + 10,(uint8_t*)len_ptr,4);
+
+	void *code = mmap(NULL, sizeof(buf), PROT_READ|PROT_WRITE, 
+		MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+	
+	if(code == MAP_FAILED) {
+		fprintf(stderr, "mmap() failed\n");
+		return -1;
+	}
+
+	memcpy(code, buf, sizeof(buf));
+
+	if(mprotect(code, sizeof(buf), PROT_EXEC) < 0){
+		fprintf(stderr, "mprotect failed to mark exec-only\n");
+		return -1;
+	}
+
+	func_t func = (func_t)(uint64_t)code;
+	func();
+
+	return 0;
+}
 
 int main(){
 
-	const size_t len = sizeof(buf);
-	unsigned long addr = (unsigned long)message;
-
-	void *hw_code = mmap(NULL, len, PROT_READ|PROT_WRITE, 
-		MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-	
-	if(hw_code == MAP_FAILED) {
-		fprintf(stderr, "mmap() failed\n");
-		return EXIT_FAILURE;
-	}
-
-	memcpy(hw_code, buf, len);
-
-	if(mprotect(hw_code, len, PROT_EXEC) < 0){
-		fprintf(stderr, "mprotect failed to mark exec-only\n");
-		return EXIT_FAILURE;
-	}
-
-	func_t func = (func_t)(uint64_t)hw_code;
-	func(addr);
-	printf("Done!\n");
+	print("Hello World!\n");
+	print("BAKA!\n");
 
 	return 0;
 }
